ab68bf8984cc8d931fa2e1ba67a138dded53b1cd
matuigle <maattuuigle@gmail.com>
2024-10-17 07:35:31 -0300

    .

--- bebidas.js
+++ bebidas.js
@@ -1,3 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.getElementById('menu-btn');
+    const sidebar = document.getElementById('sidebar');
+
+    menuBtn.addEventListener('click', function() {
+        if (sidebar.classList.contains('show')) {
+            sidebar.classList.remove('show');
+            sidebar.classList.add('hide');
+        } else {
+            sidebar.classList.remove('hide');
+            sidebar.classList.add('show');
+        }
+    });
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const menuItems = document.querySelectorAll('.menu-item');
     const popup = document.getElementById('popup');
